0939d24de146cfa004a4a350c640ade717c44153
BT_1N3_01 <vtran6971@bths.edu>
2018-12-20 12:27:51 -0500

    DistibuteCurrentPlayers completed program tokenpass is finished.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	//Create a testGame using the contstructor
         TokenPass testGame = new TokenPass(10);
-
         int[] testBoard = testGame.getBoard();
+
         //Print the board to see what's in it.
         System.out.println("Constructed a testBoard:");
         for (int i = 0; i < testBoard.length; i++)
@@ -14,7 +14,9 @@ public class Main {
             System.out.print(testBoard[i]+" ");
         }
         System.out.println();
+
         //Now test distributeCurrentPlayerTokens
+        //and prints out board after token distribution
         testGame.distributeCurrentPlayerTokens();
         System.out.println("After distributing tokens from player " + testGame.getCurrentPlayer()+": ");
         for(int i = 0; i < testBoard.length; i++)
--- src/com/company/TokenPass.java
+++ src/com/company/TokenPass.java
@@ -2,31 +2,34 @@ package com.company;
 
 public class TokenPass {
     private int playerCount;
-    private int[] board;
+    public int[] board;
     private int random;
     private int currentPlayer;
     private int tokens;
 
+    //Constructor of tokenpass passing in the playercount desired
     public TokenPass(int playerCount)
     {
      this.playerCount = playerCount;
      currentPlayer = (int)(Math.random()*playerCount);
     }
 
+    //getBoard method that runs through the entire board and fills it with a random number of tokens up to 10
     public int[] getBoard() {
-        int[] board = new int[playerCount];
+        this.board = new int[playerCount];
         for (int i = 0; i < playerCount; i++)
         {
-            random = (int) (Math.random()*10)+1;
+            random = (int) (Math.random()*9)+1;
             board[i] = random;
-            System.out.println(board[i]);
         }
         return board;
     }
 
+    //Distributes tokens one by one taking the tokens away from the currentPlayer
+    //Starting distribution with the next player
+    //Method ends when number of tokens hits zero
 public void distributeCurrentPlayerTokens()
-    {
-        int[] board = getBoard();
+{
      tokens = board[currentPlayer];
      currentPlayer += 1;
      if(tokens > 0) {
@@ -36,6 +39,7 @@ public void distributeCurrentPlayerTokens()
              tokens--;
              if (tokens == 0)
              {
+                 board[currentPlayer] = tokens;
                  j = playerCount;
              }
          }
