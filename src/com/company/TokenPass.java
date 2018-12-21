309c32fe2cf94c28eef03d45d37d4da0ebf6ab30
Vincent Tran <vtran6971@bths.edu>
2018-12-20 20:33:24 -0500

    Refinement

--- src/com/company/TokenPass.java
+++ src/com/company/TokenPass.java
@@ -19,10 +19,14 @@ public class TokenPass {
         this.board = new int[playerCount];
         for (int i = 0; i < playerCount; i++)
         {
-            random = (int) (Math.random()*9)+1;
+            random = (int) (Math.random()*10);
             board[i] = random;
         }
         return board;
+    }
+
+    public int getCurrentPlayer() {
+        return currentPlayer;
     }
 
     //Distributes tokens one by one taking the tokens away from the currentPlayer
@@ -30,25 +34,32 @@ public class TokenPass {
     //Method ends when number of tokens hits zero
 public void distributeCurrentPlayerTokens()
 {
+    currentPlayer -= 1;
      tokens = board[currentPlayer];
-     currentPlayer += 1;
-     if(tokens > 0) {
-         for (int j = currentPlayer; j < playerCount; j++)
-         {
-             board[j] = board[j]+1;
-             tokens--;
-             if (tokens == 0)
-             {
-                 board[currentPlayer] = tokens;
-                 j = playerCount;
-             }
-         }
-     }
+     board[currentPlayer] = 0;
+    if(currentPlayer == 9)
+    {
+        currentPlayer = 0;
     }
+    else {
+        currentPlayer += 1;
+    }
+if(tokens > 0) {
+    for (int j = currentPlayer; j < playerCount; j++) {
+        board[j] = board[j] + 1;
+        tokens--;
+        if (j == 9) {
+            j = 0;
+            board[j] = board[j] + 1;
+            tokens--;
+        }
+        if (tokens == 0) {
+            j = playerCount;
+        }
+    }
+}
 
-    public int getCurrentPlayer() {
-        return currentPlayer;
-    }
+}
 }
 
 
